2f0ae70543b4465d60122b37984e3e6c9e9b8163
Breakout Bandits <144791450+breakoutbandits@users.noreply.github.com>
2026-01-21 11:58:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+// Oude code - app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 // ---- Brevo API mailer (geen Nodemailer nodig)
 async function sendWarningViaBrevoAPI({ subject, html }) {
